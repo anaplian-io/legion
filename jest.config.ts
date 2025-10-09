59c8ce877b2cadda4e77869b4a6a3f65b461b2b8
Ethan Fortner <30123404+efortner@users.noreply.github.com>
2025-10-08 23:12:05 -0500

    feat: initial repo bootstrapping (#2)
    Co-authored-by: Ethan Fortner <ethan@anaplian.io>

---
+++ jest.config.ts
@@ -0,0 +1,30 @@
+export default {
+  preset: 'ts-jest/presets/default-esm',
+  testEnvironment: 'node',
+  extensionsToTreatAsEsm: ['.ts'],
+  transform: {
+    '^.+\\.ts$': ['ts-jest', { useESM: true, tsconfig: 'tsconfig.jest.json' }],
+  },
+  testMatch: ['<rootDir>/src/**/*.test.ts'],
+  roots: ['<rootDir>/src'],
+  moduleFileExtensions: ['ts', 'js', 'json', 'node'],
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageReporters: ['json', 'lcov', 'text', 'clover'],
+  collectCoverageFrom: ['src/**/*.ts'],
+  coveragePathIgnorePatterns: [
+    '/node_modules/',
+    '/dist/',
+    'index.ts',
+    '\\.guard\\.ts$',
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
+  transformIgnorePatterns: ['/node_modules/'],
+};
